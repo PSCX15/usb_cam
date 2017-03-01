8c5f0b138c5b7949edffded13d2e761d40c17371
Arpafaucon <arpafaucon@gmail.com>
2017-03-01 13:45:14 +0100

    zeé

---
+++ camcalib.sh
@@ -0,0 +1,1 @@
+rosrun camera_calibration cameracalibrator.py --size=8x6 --square=0.0205 --approximate=0.5 right:=/stereo/right/image_raw left:=/stereo/left/image_raw right_camera:=/stereo/right left_camera:=/stereo/left
